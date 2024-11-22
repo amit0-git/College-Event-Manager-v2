ea01bcc03deff2b3e039dfc5f32b51f3978d2e06
Amit Verma <root.avanti@gmail.com>
2024-11-23 02:03:16 +0530

    First Deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,14 +6,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:11000', // Your backend server URL
-          changeOrigin: true, // Allows the proxy to change the origin of the request
-          rewrite: (path) => path.replace(/^\/api/, ''), // Rewrite the path to remove '/api'
+        proxy: {
+            '/api': {
+                target: 'http://localhost:11000', // Your backend server URL
+                changeOrigin: true, // Allows the proxy to change the origin of the request
+                rewrite: (path) => path.replace(/^\/api/, ''), // Rewrite the path to remove '/api'
+            },
         },
-      },
     },
-  });
-  
+    build: {
+        rollupOptions: {
+            external: ['@fortawesome/free-solid-svg-icons']
+        }
+    }
+});
+
 
