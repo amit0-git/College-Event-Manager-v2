436a9c5e14cfc9a3d9cbbdc44972130d015ec265
Amit Verma <root.avanti@gmail.com>
2024-11-23 01:36:29 +0530

    First Deploy

---
+++ client/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+
+
+export default defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:11000', // Your backend server URL
+          changeOrigin: true, // Allows the proxy to change the origin of the request
+          rewrite: (path) => path.replace(/^\/api/, ''), // Rewrite the path to remove '/api'
+        },
+      },
+    },
+  });
+  
+
